17dd8bf5aae95d509df55c6be93a6eda2505f090
igorg <igorg@oculus.com>
2019-03-02 23:13:18 -0800

    patched with heavily reworked xrdb

--- dwm.c
+++ dwm.c
@@ -35,6 +35,7 @@
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
 #include <X11/Xproto.h>
+#include <X11/Xresource.h>
 #include <X11/Xutil.h>
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
@@ -56,6 +57,21 @@
 #define HEIGHT(X)               ((X)->h + 2 * (X)->bw + gappx)
 #define TAGMASK                 ((1 << LENGTH(tags)) - 1)
 #define TEXTW(X)                (drw_fontset_getwidth(drw, (X)) + lrpad)
+#define XRDB_LOAD_COLOR(R,V)    if (XrmGetResource(xrdb, R, "String", &type, &value) == True) { \
+                                  if (value.addr != NULL && strnlen(value.addr, 8) == 7 && value.addr[0] == '#') { \
+                                    int i = 1; \
+                                    for (; i <= 6; i++) { \
+                                      if (value.addr[i] < 48) break; \
+                                      if (value.addr[i] > 57 && value.addr[i] < 65) break; \
+                                      if (value.addr[i] > 70 && value.addr[i] < 97) break; \
+                                      if (value.addr[i] > 102) break; \
+                                    } \
+                                    if (i == 7) { \
+                                      strncpy(V, value.addr, 7); \
+                                      V[7] = '\0'; \
+                                    } \
+                                  } else fprintf(stderr, "dwm: color value parsing failed\n"); \
+                                } else fprintf(stderr, "dwm: XrmGetResource failed\n");
 
 #define SYSTEM_TRAY_REQUEST_DOCK    0
 
@@ -204,6 +220,7 @@ static void grabkeys(void);
 static void incnmaster(const Arg *arg);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
+static void loadxrdb(void);
 static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
@@ -267,6 +284,7 @@ static Client *wintosystrayicon(Window w);
 static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
+static void xrdb(const Arg *arg);
 static void zoom(const Arg *arg);
 
 /* variables */
@@ -1138,6 +1156,27 @@ killclient(const Arg *arg)
 		XUngrabServer(dpy);
 	}
 }
+
+void
+loadxrdb()
+{
+  XrmDatabase xrdb = XrmGetFileDatabase(xres);
+  if (xrdb != NULL) {
+    char *type;
+    XrmValue value;
+
+    XRDB_LOAD_COLOR("dwm.normbordercolor", normbordercolor);
+    XRDB_LOAD_COLOR("dwm.normbgcolor", normbgcolor);
+    XRDB_LOAD_COLOR("dwm.normfgcolor", normfgcolor);
+    XRDB_LOAD_COLOR("dwm.selbordercolor", selbordercolor);
+    XRDB_LOAD_COLOR("dwm.selbgcolor", selbgcolor);
+    XRDB_LOAD_COLOR("dwm.selfgcolor", selfgcolor);
+    XRDB_LOAD_COLOR("dwm.titlebordercolor", titlebordercolor);
+    XRDB_LOAD_COLOR("dwm.titlebgcolor", titlebgcolor);
+    XRDB_LOAD_COLOR("dwm.titlefgcolor", titlefgcolor);
+  }
+}
+
 
 void
 manage(Window w, XWindowAttributes *wa)
@@ -2494,6 +2533,27 @@ systraytomon(Monitor *m) {
 }
 
 void
+xrdb(const Arg *arg)
+{
+  loadxrdb();
+
+  drw_clr_create(drw, &scheme[SchemeNorm ][ColBorder], normbordercolor);
+  drw_clr_create(drw, &scheme[SchemeNorm ][ColBg    ], normbgcolor);
+  drw_clr_create(drw, &scheme[SchemeNorm ][ColFg    ], normfgcolor);
+
+  drw_clr_create(drw, &scheme[SchemeSel  ][ColBorder], selbordercolor);
+  drw_clr_create(drw, &scheme[SchemeSel  ][ColBg    ], selbgcolor);
+  drw_clr_create(drw, &scheme[SchemeSel  ][ColFg    ], selfgcolor);
+
+  drw_clr_create(drw, &scheme[SchemeTitle][ColBorder], titlebordercolor);
+  drw_clr_create(drw, &scheme[SchemeTitle][ColBg    ], titlebgcolor);
+  drw_clr_create(drw, &scheme[SchemeTitle][ColFg    ], titlefgcolor);
+
+  focus(NULL);
+  arrange(NULL);
+}
+
+void
 zoom(const Arg *arg)
 {
 	Client *c = selmon->sel;
@@ -2524,6 +2584,7 @@ main(int argc, char *argv[])
 	if (pledge("stdio rpath proc exec", NULL) == -1)
 		die("pledge");
 #endif /* __OpenBSD__ */
+	xrdb(NULL);
 	scan();
 	run();
 	cleanup();
