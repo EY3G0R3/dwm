4fe20f229c68a34505c123be5d6605238260a968
igorg <igorg@fb.com>
2019-03-11 18:31:46 -0700

    personalizations: use 14px font instead of 13px after moving to Terminess

--- config.def.h
+++ config.def.h
@@ -12,8 +12,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 
-static const char *fonts[]          = { "monospace:pixelsize=13" };
-static const char dmenufont[]       = "monospace:pixelsize=13";
+static const char *fonts[]          = { "monospace:pixelsize=14" };
+static const char dmenufont[]       = "monospace:pixelsize=14";
 
 static char normbordercolor[]  = "#928374";
 static char normbgcolor[]      = "#282828";  // gruvbox dark bg
