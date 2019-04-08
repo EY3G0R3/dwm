dfa54bf230532d944a56081a978992de6548efc5
igorg <fresh13@gmail.com>
2019-04-07 23:13:39 -0700

    Revert "taught fancybar to respect SchemeTitle"
    This reverts commit 3cbc75260684ad4866dd7e62507d82c664ea0267.

--- dwm.c
+++ dwm.c
@@ -905,9 +905,9 @@ drawbar(Monitor *m)
 					continue;
 				tw = MIN(m->sel == c ? w : mw, TEXTW(c->name));
 
-				drw_setscheme(drw, scheme[SchemeTitle]);
+				drw_setscheme(drw, scheme[m->sel == c ? SchemeSel : SchemeNorm]);
 				if (tw > 0) /* trap special handling of 0 in drw_text */
-					drw_text(drw, x, 0, tw, bh, lrpad / 2, c->name, m->sel == c);
+					drw_text(drw, x, 0, tw, bh, lrpad / 2, c->name, 0);
 				if (c->isfloating)
 					drw_rect(drw, x + boxs, boxs, boxw, boxw, c->isfixed, 0);
 				x += tw;
