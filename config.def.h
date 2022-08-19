d32fcceb39261ad20e98130287c50c5257470053
igorg <igorg@fb.com>
2022-08-19 05:30:26 -0700

    [defaultapps] q: launches dwm-sensible-terminal

--- config.def.h
+++ config.def.h
@@ -117,7 +117,7 @@ static const char *tags[] = {
 
 
 static const char *defaultapps[] = { // has to contain the same number of entries as tags[]
-	NULL, 		// "code-fb",	// q: code
+	"dwm-sensible-terminal",	// q: code
 	"google-chrome",// w: web 			e: english layout
 	"WorkChat",	// s: slack (WorkChat)
 	"Messenger",	// d: dm (Messenger)		f: toggle fullscreen
